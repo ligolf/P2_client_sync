01d20d4d54f038bb17a8fce67386ee74816c3d62
gilllian ginsberg <gillian.ginsberg@gmail.com>
2019-07-07 15:55:58 -0700

    I got the to do items sheet built.  needs modification for handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,39 +1,6 @@
 ###
 Schema
 
-CREATE DATABASE clientSync_db;
+DROP DATABASE IF EXISTS clientSync_db;
 USE clientSync_db;
 
-CREATE TABLE inventory
-(
-    id int NOT NULL
-    AUTO_INCREMENT,
-	catogory varchar
-    (255) NOT NULL,
-	description varchar
-    (255) NOT NULL,
-    make varchar
-    (255) NOT NULL,
-    model varchar
-    (255) NOT NULL,
-	PRIMARY KEY
-    (id)
-);
-
-    CREATE TABLE contacts
-    (
-        id int NOT NULL
-        AUTO_INCREMENT,
-	profession varchar
-        (255) NOT NULL,
-	firstName varchar
-        (255) NOT NULL,
-    lastName varchar
-        (255) NOT NULL,
-    phone varchar
-        (255) NOT NULL,
-    email varchar
-        (255) NOT NULL,
-	PRIMARY KEY
-        (id)
-);
